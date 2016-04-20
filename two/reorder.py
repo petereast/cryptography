4642d61f08d8bd7e63bec6309ce88e4a4591837e
Peter East <08peast@gmail.com>
2016-04-20 10:00:13 +0100

    working on it!

--- two/reorder.py
+++ two/reorder.py
@@ -1,12 +1,8 @@
 # Reorder a pylist according to a key so it appears to be random, but can
 # be reconstructed in it's original order
 
-testing_list = [x for x in range(0, 100)]
+testing_list = [x for x in range(100)]
 key = bytes("Hello", "UTF-8")
-
-def reorder(input_list, key):
-	print("Reordering list...")
-
 
 # RECURSIVE BLOCKS!!!
 
@@ -31,6 +27,16 @@ def strip_list(inlist):
 	return inlist
 
 #TODO: make this more efficient! right now it's O(N^N)
+
+def calculate_blocksize(input_list, key):
+	a, b = len(input_list), len(key)
+	# Somehow calculate a blocksize which is proportional to the size of the
+	# input_list and key, but isn't larger than {whatever} fraction of the
+	# input_list
+print(calculate_blocksize(testing_list, key))
+
+def reorder(input_list, key):
+	print("Reordering list...")
 
 print(sepblocks(testing_list.copy(), 2))
 print(testing_list)
