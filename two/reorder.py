3ed102e4c9a603c002fca19ffbfc615795ec8812
Peter East <08peast@gmail.com>
2016-04-27 16:08:06 +0100

    trsfw

--- two/reorder.py
+++ two/reorder.py
@@ -33,10 +33,12 @@ def calculate_blocksize(input_list, key):
 	# Somehow calculate a blocksize which is proportional to the size of the
 	# input_list and key, but isn't larger than {whatever} fraction of the
 	# input_list
+	return 3
 print(calculate_blocksize(testing_list, key))
 
 def reorder(input_list, key):
 	print("Reordering list...")
+	# This will recursively take each sublist and work it's way along the tree of values
 
 print(sepblocks(testing_list.copy(), 2))
 print(testing_list)
